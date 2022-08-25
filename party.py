d5b80e5b860b6ea3eafe91eb0826713a21c5ed24
Rebecca Gordon <14rebeccagordon@gmail.com>
2022-08-25 16:54:55 -0700

    Finished Further Study

--- party.py
+++ party.py
@@ -7,13 +7,43 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+    
+    >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+    True
+    >>> is_mel('Mel Melitpolski','judith@awesome.com')
+    True
+    >>> is_mel('Tavish', 'mel@ubermelon.com')
+    True
+    >>> is_mel('Judith Butler', 'judith@awesome.com')
+    False
+    >>> is_mel('MEL MELITPOLSKI','mel@ubermelon.com')
+    True
+    >>> is_mel('Bec', 'MEL@UBERMELON.COM')
+    True
 
-    return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
+    """
+    name = name.lower()
+    email = email.lower()
+
+    return name == "mel melitpolski" or email == "mel@ubermelon.com" or name =="mel"
 
 
 def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
+
+    >>> treats = [{"type": "soda"},{"type":"fruit"},{"type":"soda"}]
+    >>> most_and_least_common_type(treats)
+    ('soda', 'fruit')
+
+    >>> treats = [{"type": "soda"},{"type": "soda"},]
+    >>> most_and_least_common_type(treats)
+    ('soda', 'soda')
+
+    >>> treats = []
+    >>> most_and_least_common_type(treats)
+    (None, None)
+
 
     Return most and least common treat types in tuple of format (most, least).
     """
